39e1f0d75731432ce5965a590c56c0a0fda9e064
Nireekshaamin <nireekshaamin2000@gmail.com>
2022-10-12 17:40:37 +0530

    Add project files.

---
+++ DFA_CORE/Models/Trainee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace DFA_CORE.Models
+{
+    public partial class Trainee
+    {
+        [Key]
+        [Required(ErrorMessage = "Id is required")]
+        public int Tid { get; set; }
+        [Required(ErrorMessage ="Name is required")]
+        [MaxLength(15,ErrorMessage ="Max length should be 15")]
+        [MinLength(3, ErrorMessage = "Min length should be 3")]
+        public string? Tname { get; set; }
+        [Required(ErrorMessage = "Age is required")]
+        [Range(23,100,ErrorMessage ="Age should be more than 18")]
+        public int? Tage { get; set; }
+    }
+}
